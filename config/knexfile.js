041c8586a697bf516a67c1500c10ed45aa26c202
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-02-17 23:48:48 -0800

    SSL

--- config/knexfile.js
+++ config/knexfile.js
@@ -5,6 +5,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
     migrations: { directory: '../data/migrations' },
     seeds: { directory: '../data/seeds' },
     pool: {
