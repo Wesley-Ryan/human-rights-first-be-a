bcd457d5066e84f9f18d977682b009d0995c0073
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-02-18 00:47:54 -0800

    SSL FILE

--- config/knexfile.js
+++ config/knexfile.js
@@ -22,10 +22,8 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection:
-      'postgres://ztbbehmwoxabnw:92a9fee1e8dba1f8df2f1483557235a6875a43c611da7d3d61e36b6d48c97239@ec2-35-174-118-71.compute-1.amazonaws.com:5432/d5l16f5si8l56sssl=true',
+    connection: process.env.DATABASE_URL,
     ssl: {
-      require: true,
       rejectUnauthorized: false,
     },
     migrations: { directory: '../data/migrations' },
