85db08f225521cee1983551dd97094a733b36c04
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-02-23 22:31:13 -0800

    Created POST added twitterIncidentMiddleware

--- api/twitter_incidents/twitterIncidentsRouter.js
+++ api/twitter_incidents/twitterIncidentsRouter.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const {
+  validateTwitterPost,
+} = require('../middleware/twitterIncidentValidations');
 // Model and util imports
 const twitterIncidentHelper = require('./twitterIncidentsModel');
 
@@ -31,18 +34,37 @@ router.get('/incidents/approved', async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
-router.put('/incidents/:id', async (req, res) => {
+router.put('/incidents/:id', validateTwitterPost, async (req, res) => {
   const { id } = req.params;
-  const changes = req.body;
+  const changes = req.Twitter;
   try {
     const updatedTwitterIncident = await twitterIncidentHelper.updateTwitterIncident(
       id,
       changes
     );
+    if (updatedTwitterIncident.length < 1) {
+      res.status(400).json({
+        message:
+          'ERROR: The incident requested does not exist. Please choose a valid incident.',
+      });
+    }
     twitterIncidentHelper.cleanTwitterIncident(updatedTwitterIncident);
     res.status(200).json(updatedTwitterIncident);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+});
+router.post('/incidents', validateTwitterPost, async (req, res) => {
+  try {
+    const newPostedIncident = await twitterIncidentHelper.createTwitterIncident(
+      req.Twitter
+    );
+    res.status(201).json(newPostedIncident);
+  } catch (error) {
+    res.status(500).json({
+      message: 'There was a problem with creating your issue',
+      Error_message: error.message,
+    });
   }
 });
 
