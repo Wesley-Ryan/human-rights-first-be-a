af942f825037964440f35da392c8d126bc1339ec
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-03-01 15:46:16 -0800

    Corrected twitter incident error

--- api/twitter_incidents/twitterIncidentsModel.js
+++ api/twitter_incidents/twitterIncidentsModel.js
@@ -1,91 +1,98 @@
-const express = require('express');
-const router = express.Router();
-const {
-  validatePostBody,
-  addIdtoPost,
-  validateManyPosts,
-} = require('../middleware/twitterIncidentValidations');
-const authRequired = require('../middleware/authRequired');
+const db = require('../../data/db-config');
 
-// Model and util imports
-const twitterIncidentHelper = require('./twitterIncidentsModel');
+module.exports = {
+  getAllPendingIncidents,
+  getAllRejectedIncidents,
+  getAllApprovedIncidents,
+  getLearningData,
+  getLastID,
+  getTwitterIncidentById,
+  updateTwitterIncident,
+  createTwitterIncident,
+};
 
-router.get('/incidents', async (req, res) => {
-  try {
-    const twitterAdminFeed = await twitterIncidentHelper.getAllPendingIncidents();
-    res.status(200).json(twitterAdminFeed);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+/**
+ * Returns all pending Twitter incidents in the db sorted by newest incident first
+ */
+function getAllPendingIncidents() {
+  return db('twitter_incidents')
+    .where({ pending: true })
+    .orderBy('date', 'desc');
+}
+/**
+ * Returns all rejected Twitter incidents in the db sorted by newest incident first
+ */
+function getAllRejectedIncidents() {
+  return db('twitter_incidents')
+    .where({ rejected: true })
+    .orderBy('date', 'desc');
+}
+/**
+ * Returns all approved Twitter incidents in the db sorted by newest incident first
+ */
+function getAllApprovedIncidents() {
+  return db('twitter_incidents')
+    .where({ approved: true })
+    .orderBy('date', 'desc');
+}
+/**
+ * Returns all approved/rejected Twitter incidents for DS Training Data
+ */
+function getLearningData() {
+  return db('twitter_incidents')
+    .whereNot({ pending: true })
+    .orderBy('date', 'desc');
+}
 
-router.get('/incidents/rejected', async (req, res) => {
-  try {
-    const twitterRejectedAdminFeed = await twitterIncidentHelper.getAllRejectedIncidents();
-    res.status(200).json(twitterRejectedAdminFeed);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-router.get('/incidents/approved', async (req, res) => {
-  try {
-    const twitterApprovedAdminFeed = await twitterIncidentHelper.getAllApprovedIncidents();
-    res.status(200).json(twitterApprovedAdminFeed);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-router.get('/incidents/dstraining', async (req, res) => {
-  try {
-    const learningData = await twitterIncidentHelper.getLearningData();
-    res.status(200).json(learningData);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+/**
+ * Returns the last known id in the database
+ */
+function getLastID() {
+  return db('twitter_incidents').max('id');
+}
+/**
+ * @param {string} id
+ * Function to return a specific Twitter incident by provided id
+ */
+function getTwitterIncidentById(id) {
+  return db('twitter_incidents').where('id', id);
+}
 
-router.put('/incidents/:id', validatePostBody, async (req, res) => {
-  const { id } = req.params;
-  const changes = req.newIncident;
+/**
+ * @param {string} id
+ * @param {Object} changes
+ * Function to Edit and return a specific Twitter incident by provided id
+ */
+async function updateTwitterIncident(id, changes) {
   try {
-    const updatedTwitterIncident = await twitterIncidentHelper.updateTwitterIncident(
-      id,
-      changes
-    );
-    if (updatedTwitterIncident.length < 1) {
-      res.status(400).json({
-        message:
-          'ERROR: The incident requested does not exist. Please choose a valid incident.',
-      });
-    }
-    res.status(201).json(updatedTwitterIncident);
+    await db('twitter_incidents').where('id', id).update(changes);
+    return db('twitter_incidents').where('id', id);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    throw new Error(error.message);
   }
-});
-router.put('/incidents/', async (req, res) => {
-  const changes = req.body;
-  try {
-    await changes.forEach((change) => {
-      twitterIncidentHelper.updateTwitterIncident(change.id, change);
-    });
-    res.status(201).json({ message: 'Incidents Successfully Updated' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-router.post('/incidents', validatePostBody, addIdtoPost, async (req, res) => {
-  try {
-    const newPostedIncident = await twitterIncidentHelper.createTwitterIncident(
-      req.TwitterUpdatedReadyToPost
-    );
-    res.status(201).json(newPostedIncident);
-  } catch (error) {
-    res.status(500).json({
-      message: 'There was a problem with creating your issue',
-      Error_message: error.message,
-    });
-  }
-});
+}
 
-module.exports = router;
+/**
+ * @param {Object} incident
+ * Function to Edit and return a specific Twitter incident by provided id
+ */
+async function createTwitterIncident(incident) {
+  try {
+    await db('twitter_incidents').insert(incident);
+    return 'New incident Created';
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+/**
+ * @param {Object} twitterIncident
+ * Retuns Twitter incident that matches the shape of the Reddit incident
+ */
+
+function cleanTwitterIncident(twitterIncident) {
+  twitterIncident.map((incident) => {
+    incident.src = JSON.parse(incident.src);
+    incident.categories = JSON.parse(incident.categories);
+    return incident;
+  });
+}
